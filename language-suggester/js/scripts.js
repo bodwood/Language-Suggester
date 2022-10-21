f868faeba7f46092f93b1a620d16ed6ce73fcec4
bodwood <bodiewood@hotmail.com>
2022-10-21 13:18:57 -0700

    displays java when user selects smartphones and windows

--- language-suggester/js/scripts.js
+++ language-suggester/js/scripts.js
@@ -13,28 +13,32 @@ function handleSuggester(event){
   const java = document.querySelector("div#java");
   const swift = document.querySelector("div#swift");
   const cPlus = document.querySelector("div#cPlus");
+  const formBody = document.querySelector("div#formBody");
 
   python.setAttribute("class", "hidden");
   java.setAttribute("class", "hidden");
+  formBody.setAttribute("class", "reveal");
 
 
-  if(platform === "smartPhones"){
-    python.removeAttribute("class", "hidden");
+
+  if(platform === "smartPhones" && perferredOS === "windows"){
     java.removeAttribute("class", "hidden");
+    formBody.setAttribute("class", "hidden");
+  } else{
+
   }
-
-  let btnClear = document.getElementById("btn");
-  btnClear.addEventListener('click', function handleClick(event) {
-    event.preventDefault();
-    platform.value = '';
-    videoGame.value = '';
-  });
+   
+  resetPage();
+  
 
 
+}
+
+function resetPage(){
+  document.getElementById("suggester").reset();
 }
 
 window.addEventListener("load", function() {
   const form = document.getElementById("suggester");
   form.addEventListener("submit", handleSuggester);
-  form.reset();
 });
